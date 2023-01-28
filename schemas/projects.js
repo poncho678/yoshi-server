5e131c6ff384c9062b2112e671ad6d32156ecb97
poncho678 <vincent.tollens@googlemail.com>
2023-01-28 19:10:29 +0100

    changed client to Broadcaster

--- schemas/projects.js
+++ schemas/projects.js
@@ -67,7 +67,7 @@ export default {
       group: 'media',
     },
     {
-      title: 'Client',
+      title: 'Broadcaster',
       name: 'client',
       type: 'string',
       group: ['basic', 'details'],
